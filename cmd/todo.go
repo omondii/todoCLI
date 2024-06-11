35559146d0c0ca33da1d2586af3b5daa38a72cdf
Brian Omondi <omondiibrian00@gmail.com>
2024-06-11 20:54:34 +0300

    Initial commit

---
+++ cmd/todo.go
@@ -0,0 +1,36 @@
+package todo
+
+import (
+	"errors"
+	"time"
+)
+
+type item struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) Add(task string) {
+	todo := item{
+		Task:        task,
+		Done:        false,
+		CreatedAt:   time.Now(),
+		CompletedAt: time.Time{},
+	}
+	*t = append(*t, todo)
+}
+
+func (t *Todos) Complete(index int) error {
+	if index < 0 || index >= len(*t) {
+		return errors.New("invalid index")
+	}
+
+	(*t)[index].CompletedAt = time.Now()
+	(*t)[index].Done = true
+
+	return nil
+}
